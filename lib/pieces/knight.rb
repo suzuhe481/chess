ca1e5298081bb429080e41dca65f76604d757585
Hector <baseballhector@gmail.com>
2022-06-02 21:01:47 -0400

    Initializing knight class with instance variables

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,3 +1,14 @@
+# The class for a knight piece in chess.
+# Knight movement: Moves in an L-shape where it moves 2 squares vertically and 1 square horizontally, or
+#                  2 squares horizontally and 1 square vertically.
+#                  This piece is allowed to jump over other pieces.
+# Capture: Can capture a opponent's piece if it lands on it.
 class Knight
-  def initialize; end
+  attr_accessor :rank, :file, :position
+
+  def initialize(file, rank)
+    @file = file
+    @rank = rank
+    @position = [file, rank]
+  end
 end
