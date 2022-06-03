7a8c81805da98fa6f2b3539cc977c7678aef80ff
Hector <baseballhector@gmail.com>
2022-06-02 21:50:08 -0400

    Initializing pawn with instance variables

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,3 +1,13 @@
+# This class is for the pawn piece in chess.
+# Pawn movement: Moves 1 square forward at a time. Can move 2 squares forward only on that piece's first turn.
+# Capture: Can capture an opponent's piece if it is 1 square away, diagonally, to the right or left.
 class Pawn
-  def initialize; end
+  attr_accessor :rank, :file, :position, :first_move
+
+  def initialize(file, rank)
+    @file = file
+    @rank = rank
+    @position = [file, rank]
+    @first_move = true
+  end
 end
