6f023f9c1059342a024739dca0bdb0eeed853834
Hector <baseballhector@gmail.com>
2022-06-02 23:07:49 -0400

    Initializing king with variables

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,3 +1,14 @@
+# The king piece in chess.
+# King movement: Can move horizontally, vertically, or diagonally in any direction for only 1 square 
+# until the edge of the board.
+# Capture: Can capture an opponent's piece when landing on it.
 class King
-  def initialize; end
+  attr_accessor :rank, :file, :position, :in_check
+
+  def initialize(file, rank)
+    @file = file
+    @rank = rank
+    @position = [file, rank]
+    @in_check = false
+  end
 end
