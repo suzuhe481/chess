1398473cbba486809a01f07017bff4ed0e952f69
Hector <baseballhector@gmail.com>
2022-06-02 23:02:21 -0400

    Initializing queen with variables

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,3 +1,13 @@
+# This class is for the queen piece in chess.
+# Queen movement: Can move horizontally, vertically, or diagonally in any direction for any number 
+# of squares until the edge of the board.
+# Capture: Can capture an opponent's piece by landing on it.
 class Queen
-  def initialize; end
+  attr_accessor :rank, :file, :position
+
+  def initialize(file, rank)
+    @file = file
+    @rank = rank
+    @position = [file, rank]
+  end
 end
