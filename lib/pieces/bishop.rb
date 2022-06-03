6e4dec7d5b9a8f57f02cd5c591b1277c8b19599e
Hector <baseballhector@gmail.com>
2022-06-02 22:14:51 -0400

    Initializing bishop with variables

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,3 +1,12 @@
+# This class is for the bishop piece in chess.
+# Bishop movement: This piece can move digonally in any direction until the edge of the board.
+# Capture: Can capture an opponent's piece when landing on it.
 class Bishop
-  def initialize; end
+  attr_accessor :rank, :file, :position
+
+  def initialize(file, rank)
+    @file = file
+    @rank = rank
+    @position = [file, rank]
+  end
 end
