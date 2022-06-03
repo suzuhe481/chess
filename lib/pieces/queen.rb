295a93dc614bf7cf21d888c6ccc40f080f2f392e
Hector <baseballhector@gmail.com>
2022-06-02 23:05:00 -0400

    Added movement method

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -10,4 +10,24 @@ class Queen
     @rank = rank
     @position = [file, rank]
   end
+
+  # Returns every valid or invalid move as an array from this piece's position.
+  def movement
+    all_moves = []
+    max_range = (1..7)
+
+    max_range.each do |spaces|
+      all_moves.append( [(file.ord + spaces).chr, rank] )
+      all_moves.append( [(file.ord - spaces).chr, rank] )
+      all_moves.append( [(file.ord).chr, rank + spaces] )
+      all_moves.append( [(file.ord).chr, rank - spaces] )
+
+      all_moves.append( [(file.ord + spaces).chr, rank + spaces] )
+      all_moves.append( [(file.ord - spaces).chr, rank - spaces] )
+      all_moves.append( [(file.ord + spaces).chr, rank - spaces] )
+      all_moves.append( [(file.ord - spaces).chr, rank + spaces] )
+    end
+
+    all_moves
+  end
 end
